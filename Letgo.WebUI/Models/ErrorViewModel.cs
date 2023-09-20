b45ff1159fcb5235d370709c8bd5341d84f6810b
mrtcnck <mrtcnck@hotmail.com>
2023-09-20 19:19:48 +0300

    MVC Projesi oluşturuldu

---
+++ Letgo.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Letgo.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
